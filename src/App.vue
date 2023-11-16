b8efbedaffb771dd487eae82c98e75e42e2978e4
Veni Muthu <venimuthu@Installer-s-MacBook-Pro.local>
2023-11-16 09:03:57 -0600

    Adding TO-DO project files to git

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import TodoApp from '@/components/TodoApp.vue'
+import TheWelcome from './components/TheWelcome.vue'
+
+export default {
+  name:'App',
+  components:{
+    TodoApp
+  }
+}
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div id="app">
+    Hello World
+    </div>
+  </header>
+
+  
+</template>
+
